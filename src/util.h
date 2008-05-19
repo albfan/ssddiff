4b788777c6f1f3fe88380cdeb9f6870e2d0b0284
Erich Schubert <erich@debian.org>
2008-05-19 13:01:30 +0000

    remove hash<void*> specialization that is included in TR1
    git-svn-id: svn://svn.debian.org/ssddiff/trunk@38 78e279ab-b9ee-0310-a27c-fa3b379380e8

--- src/util.h
+++ src/util.h
@@ -11,21 +11,6 @@
  * ======================================================================== */
 #ifndef  UTIL_INC
 #define  UTIL_INC
-#include <tr1/unordered_map>
-
-/**** Utilities ****/
-/* FIXME: How to best get rid of this namespace hack? */
-namespace std {
-_GLIBCXX_BEGIN_NAMESPACE(tr1)
-/** \brief trivial "hash function" for pointers */
-template<>
-struct hash<void *> {
-    /** \brief trivial "hash function" for pointers */
-    size_t operator()(const void * __x) const {
-	return reinterpret_cast<size_t>(__x); }
-    };
-_GLIBCXX_END_NAMESPACE
-}
 
 /** \brief minimum macro */
 #define MIN(a,b) (( (a<=b) ? a : b ))
